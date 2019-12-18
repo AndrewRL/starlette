79cca4ffe19a2a8ba9970e958ede5856aa217711
Andrew Laird <alaird4@gmail.com>
2019-12-18 14:16:20 -0800

    added homepage html template

--- server.py
+++ server.py
@@ -2,11 +2,15 @@
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
-from starlette.routing import Route
+from starlette.routing import Route, Mount
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='static/html')
 
 
 async def homepage(request):
-    return JSONResponse({'page': 'home'})
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
 async def posts(request):
@@ -24,5 +28,6 @@ app = Starlette(debug=True, routes=[
     Route('/', homepage),
     Route('/posts', posts),
     Route('/projects', projects),
-    Route('/about', about)
+    Route('/about', about),
+    Mount('/static', app=StaticFiles(directory='static'), name="static")
 ])
